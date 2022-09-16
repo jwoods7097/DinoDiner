c4eabefdd4d9de7b05b9e625aa30752f39d245aa
jwoods7097 <jwoods7097@gmail.com>
2022-09-16 14:17:09 -0500

    Added abstract base classes for Drinks, Entrees, and Sides

---
+++ Data/Drinks/Drink.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DinoDiner.Data.Enums;
+
+namespace DinoDiner.Data.Drinks
+{
+    /// <summary>
+    /// A base class for all drinks sold at Dino Diner
+    /// </summary>
+    public abstract class Drink
+    {
+        /// <summary>
+        /// The name of the Drink
+        /// </summary>
+        public abstract string Name { get; }
+
+        /// <summary>
+        /// The serving size of the Drink
+        /// </summary>
+        public abstract ServingSize Size { get; set; }
+
+        /// <summary>
+        /// The price of the Drink
+        /// </summary>
+        public abstract decimal Price { get; }
+
+        /// <summary>
+        /// The calories of the Drink
+        /// </summary>
+        public abstract uint Calories { get; }
+    }
+}
---
+++ Data/Sides/Side.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DinoDiner.Data.Enums;
+
+namespace DinoDiner.Data.Sides
+{
+    /// <summary>
+    /// A base class for all sides sold at DinoDiner
+    /// </summary>
+    public abstract class Side
+    {
+        /// <summary>
+        /// The name of the Side
+        /// </summary>
+        public abstract string Name { get; }
+
+        /// <summary>
+        /// The serving size of the Side
+        /// </summary>
+        public abstract ServingSize Size { get; set; }
+
+        /// <summary>
+        /// The price of the Side
+        /// </summary>
+        public abstract decimal Price { get; }
+
+        /// <summary>
+        /// The calories of the Side
+        /// </summary>
+        public abstract uint Calories { get; }
+    }
+}

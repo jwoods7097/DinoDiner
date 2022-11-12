94e39894a8c3e2c8c69da68be1c35d211a95b1b0
jwoods7097 <jwoods7097@gmail.com>
2022-11-11 20:38:13 -0600

    Added static Menu class and wrote corresponding tests

---
+++ Data/Menu.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DinoDiner.Data.Entrees;
+using DinoDiner.Data.Sides;
+using DinoDiner.Data.Drinks;
+using DinoDiner.Data.Enums;
+
+namespace DinoDiner.Data
+{
+    /// <summary>
+    /// Represents the entire menu for Dino Diner
+    /// </summary>
+    public static class Menu
+    {
+        /// <summary>
+        /// Gets a list of all the entrees served at Dino Diner
+        /// </summary>
+        public static IEnumerable<MenuItem> Entrees
+        {
+            get
+            {
+                List<MenuItem> entrees = new();
+                entrees.Add(new AllosaurusAllAmericanBurger());
+                entrees.Add(new CarnotaurusCheeseburger());
+                entrees.Add(new DeinonychusDouble());
+                entrees.Add(new TRexTriple());
+                entrees.Add(new Burger());
+                entrees.Add(new Brontowurst());
+                entrees.Add(new PrehistoricPBJ());
+                entrees.Add(new PterodactylWings());
+                entrees.Add(new VelociWraptor());
+                entrees.Add(new DinoNuggets());
+                return entrees;
+            }
+        }
+
+        /// <summary>
+        /// Gets a list of all the sides served at Dino Diner
+        /// </summary>
+        public static IEnumerable<MenuItem> Sides
+        {
+            get
+            {
+                List<MenuItem> sides = new();
+                foreach (ServingSize size in Enum.GetValues(typeof(ServingSize)))
+                {
+                    sides.Add(new Fryceritops() { Size = size });
+                    sides.Add(new MeteorMacAndCheese() { Size = size });
+                    sides.Add(new MezzorellaSticks() { Size = size });
+                    sides.Add(new Triceritots() { Size = size });
+                }
+                return sides;
+            }
+        }
+
+        /// <summary>
+        /// Gets a list of all the drinks served at Dino Diner
+        /// </summary>
+        public static IEnumerable<MenuItem> Drinks
+        {
+            get
+            {
+                List<MenuItem> drinks = new();
+                foreach (ServingSize size in Enum.GetValues(typeof(ServingSize)))
+                {
+                    foreach(SodaFlavor flavor in Enum.GetValues(typeof(SodaFlavor)))
+                    {
+                        drinks.Add(new Plilosoda() { Size = size, Flavor = flavor });
+                    }
+                    drinks.Add(new CretaceousCoffee() { Size = size });
+                }
+                return drinks;
+            }
+        }
+
+        /// <summary>
+        /// Gets a list of all items served at Dino Diner
+        /// </summary>
+        public static IEnumerable<MenuItem> FullMenu
+        {
+            get
+            {
+                List<MenuItem> menu = new();
+                menu.AddRange(Entrees);
+                menu.AddRange(Sides);
+                menu.AddRange(Drinks);
+                return menu;
+            }
+        }
+    }
+}
---
+++ DataTest/MenuUnitTests.cs
@@ -0,0 +1,126 @@
+﻿namespace DataTest
+{
+    /// <summary>
+    /// Unit Tests for Menu
+    /// </summary>
+    public class MenuUnitTests
+    {
+        /// <summary>
+        /// There should be 10 entrees in the menu
+        /// </summary>
+        [Fact]
+        public void EntreesShouldContainCorrectNumberofItems()
+        {
+            Assert.Equal(10, Menu.Entrees.Count());
+        }
+
+        /// <summary>
+        /// There should be 12 total items in Sides, 4 sides with 3 sizes each
+        /// </summary>
+        [Fact]
+        public void SidesShouldContainCorrectNumberofItems()
+        {
+            Assert.Equal(4 * 3, Menu.Sides.Count());
+        }
+
+        /// <summary>
+        /// There should be 18 total items in Drinks: 5 Plilosoda flavors with 3 sizes each,
+        /// and 3 sizes of CretaceousCoffee
+        /// </summary>
+        [Fact]
+        public void DrinksShouldContainCorrectNumberofItems()
+        {
+            Assert.Equal(5 * 3 + 3, Menu.Drinks.Count());
+        }
+
+        /// <summary>
+        /// FullMenu should have the sum of the counts of Entrees, Sides, and Drinks items
+        /// </summary>
+        [Fact]
+        public void FullMenuShouldContainCorrectNumberofItems()
+        {
+            Assert.Equal(10 + (4 * 3) + (5 * 3 + 3), Menu.FullMenu.Count());
+        }
+
+        /// <summary>
+        /// Entrees should contain the correct items
+        /// </summary>
+        [Fact]
+        public void EntreesContainsCorrectItems()
+        {
+            Assert.Collection(Menu.Entrees,
+                item => Assert.IsType<AllosaurusAllAmericanBurger>(item),
+                item => Assert.IsType<CarnotaurusCheeseburger>(item),
+                item => Assert.IsType<DeinonychusDouble>(item),
+                item => Assert.IsType<TRexTriple>(item),
+                item => Assert.IsType<Burger>(item),
+                item => Assert.IsType<Brontowurst>(item),
+                item => Assert.IsType<PrehistoricPBJ>(item),
+                item => Assert.IsType<PterodactylWings>(item),
+                item => Assert.IsType<VelociWraptor>(item),
+                item => Assert.IsType<DinoNuggets>(item)
+            );
+        }
+
+        /// <summary>
+        /// Sides should contain the correct items
+        /// </summary>
+        [Fact]
+        public void SidesContainsCorrectItems()
+        {
+            foreach(ServingSize size in Enum.GetValues(typeof(ServingSize)))
+            {
+                Assert.Contains(Menu.Sides, item => item is Fryceritops ft && ft.Size == size);
+                Assert.Contains(Menu.Sides, item => item is MeteorMacAndCheese mc && mc.Size == size);
+                Assert.Contains(Menu.Sides, item => item is MezzorellaSticks ms && ms.Size == size);
+                Assert.Contains(Menu.Sides, item => item is Triceritots tt && tt.Size == size);
+            }
+        }
+
+        /// <summary>
+        /// Drinks should contain the correct items
+        /// </summary>
+        [Fact]
+        public void DrinksContainsCorrectItems()
+        {
+            foreach (ServingSize size in Enum.GetValues(typeof(ServingSize)))
+            {
+                foreach(SodaFlavor flavor in Enum.GetValues(typeof(SodaFlavor)))
+                {
+                    Assert.Contains(Menu.Drinks, item => item is Plilosoda ps && ps.Size == size && ps.Flavor == flavor);
+                }
+                Assert.Contains(Menu.Drinks, item => item is CretaceousCoffee cc && cc.Size == size);
+            }
+        }
+
+        /// <summary>
+        /// The full menu should contain all the items from the entrees, sides, and drinks list
+        /// </summary>
+        [Fact]
+        public void FullMenuContainsCorrectItems()
+        {
+            Assert.Contains(Menu.FullMenu, item => item is AllosaurusAllAmericanBurger);
+            Assert.Contains(Menu.FullMenu, item => item is CarnotaurusCheeseburger);
+            Assert.Contains(Menu.FullMenu, item => item is DeinonychusDouble);
+            Assert.Contains(Menu.FullMenu, item => item is TRexTriple);
+            Assert.Contains(Menu.FullMenu, item => item is Burger);
+            Assert.Contains(Menu.FullMenu, item => item is Brontowurst);
+            Assert.Contains(Menu.FullMenu, item => item is PrehistoricPBJ);
+            Assert.Contains(Menu.FullMenu, item => item is PterodactylWings);
+            Assert.Contains(Menu.FullMenu, item => item is VelociWraptor);
+            Assert.Contains(Menu.FullMenu, item => item is DinoNuggets);
+            foreach (ServingSize size in Enum.GetValues(typeof(ServingSize)))
+            {
+                Assert.Contains(Menu.FullMenu, item => item is Fryceritops ft && ft.Size == size);
+                Assert.Contains(Menu.FullMenu, item => item is MeteorMacAndCheese mc && mc.Size == size);
+                Assert.Contains(Menu.FullMenu, item => item is MezzorellaSticks ms && ms.Size == size);
+                Assert.Contains(Menu.FullMenu, item => item is Triceritots tt && tt.Size == size);
+                foreach (SodaFlavor flavor in Enum.GetValues(typeof(SodaFlavor)))
+                {
+                    Assert.Contains(Menu.FullMenu, item => item is Plilosoda ps && ps.Size == size && ps.Flavor == flavor);
+                }
+                Assert.Contains(Menu.FullMenu, item => item is CretaceousCoffee cc && cc.Size == size);
+            }
+        }
+    }
+}

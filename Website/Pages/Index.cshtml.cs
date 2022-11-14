47e105c338b8055fea68588fd4e3d50094ffc8da
jwoods7097 <jwoods7097@gmail.com>
2022-11-14 17:55:51 -0600

    Added Search

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -6,6 +6,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DinoDiner.Data;
+using DinoDiner.Data.Entrees;
+using DinoDiner.Data.Sides;
+using DinoDiner.Data.Drinks;
 
 namespace Website.Pages
 {
@@ -19,17 +22,102 @@ namespace Website.Pages
         /// </summary>
         private readonly ILogger<IndexModel> _logger;
 
+        /// <summary>
+        /// The parts of the item name to search for
+        /// </summary>
+        public string SearchTerms { get; set; }
+
+        /// <summary>
+        /// The types of items to filter
+        /// </summary>
+        public string[] ItemTypes { get; set; }
+
+        /// <summary>
+        /// The minimum calories to search for
+        /// </summary>
+        public uint? CaloriesMin { get; set; }
+
+        /// <summary>
+        /// The maximum calories to search for
+        /// </summary>
+        public uint? CaloriesMax { get; set; }
+
+        /// <summary>
+        /// The minimum price to search for
+        /// </summary>
+        public decimal? PriceMin { get; set; }
+
+        /// <summary>
+        /// The maximum price to search for
+        /// </summary>
+        public decimal? PriceMax { get; set; }
+
+        /// <summary>
+        /// The filtered list of items
+        /// </summary>
+        public IEnumerable<MenuItem> Items { get; set; }
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
         /// <summary>
-        /// OnGet method
+        /// Parses HTML GET requests
         /// </summary>
-        public void OnGet()
+        /// <param name="searchTerms">The parts of the item name to search for</param>
+        /// <param name="itemTypes">The type of menu item (entree, side, or drink)</param>
+        /// <param name="caloriesMin">The minimum calories</param>
+        /// <param name="caloriesMax">The maximum calories</param>
+        /// <param name="priceMin">The minimum price</param>
+        /// <param name="priceMax">The maximum price</param>
+        public void OnGet(string searchTerms, string[] itemTypes, uint? caloriesMin, uint? caloriesMax, decimal? priceMin, decimal? priceMax)
         {
-
+            this.SearchTerms = searchTerms;
+            this.ItemTypes = itemTypes;
+            this.CaloriesMin = caloriesMin;
+            this.CaloriesMax = caloriesMax;
+            this.PriceMin = priceMin;
+            this.PriceMax = priceMax;
+            Items = Menu.FullMenu;
+            if (ItemTypes != null)
+            {
+                if(ItemTypes.Contains("Entree"))
+                {
+                    Items = Items.Where(item => item is not Entree);
+                }
+                if (ItemTypes.Contains("Side"))
+                {
+                    Items = Items.Where(item => item is not Side);
+                }
+                if (ItemTypes.Contains("Drink"))
+                {
+                    Items = Items.Where(item => item is not Drink);
+                }
+            }
+            if (SearchTerms != null)
+            {
+                foreach(string term in searchTerms.Split(" "))
+                {
+                    Items = Items.Where(item => item.Name.Contains(term, StringComparison.InvariantCultureIgnoreCase));
+                }
+            }
+            if(CaloriesMin != null)
+            {
+                Items = Items.Where(item => item.Calories >= CaloriesMin);
+            }
+            if(CaloriesMax != null)
+            {
+                Items = Items.Where(item => item.Calories <= CaloriesMax);
+            }
+            if (PriceMin != null)
+            {
+                Items = Items.Where(item => item.Price >= PriceMin);
+            }
+            if(PriceMax != null)
+            {
+                Items = Items.Where(item => item.Price <= PriceMax);
+            }
         }
     }
 }
